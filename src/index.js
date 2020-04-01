90b370565f8e82d3d3c9629760755357d78e39db
vincentLW <vincentlinweiluck@gmail.com>
2020-04-01 23:54:28 +0800

    上传

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.jpg";
+import printMe from "./print";
+
+function component() {
+  var element = document.createElement("div");
+  var btn = document.createElement("button");
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  btn.innerHTML = "Click me and check the console ssswwwwsssswwww111111";
+
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
