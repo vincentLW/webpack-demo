b3ff78fba26968a5332c845c93448fa4c9c2845a
vincentLW <vincentlinweiluck@gmail.com>
2020-04-04 21:58:25 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
+import _ from "lodash";
 
-function getComponent() {
+function component() {
+  var element = document.createElement("div");
+  var button = document.createElement("button");
+  var br = document.createElement("br");
 
-  return import(
-    /*webpackChunkName:"lodash"*/
-    "lodash"
-  )
-    .then(_ => {
-      var element = document.createElement("div");
-      element.innerHTML = _.join(["hello", "webpack"], " ");
-      return element;
-    })
-    .catch(error => "An error occurred while loading the component");
+  button.innerHTML = "Click me and look at the console!";
+  element.innerHTML = _.join(["Hello", "Webpack"]);
+  element.appendChild(br);
+  element.appendChild(button);
+
+  button.onclick = e =>
+    import(
+      /* webpackChunkName: "print" */
+      "./print"
+    ).then(module => {
+      var print = module.default;
+      print();
+    });
+
+  return element;
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
